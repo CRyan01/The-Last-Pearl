94f14ecc3a458410a41903fcd469b3372e7c19bc
Jakub <nowakjakub12@gmail.com>
2024-10-21 14:48:22 +0100

    Fixing lagging issue
    Game was lagging when creating the level in a 2D Array using pointers.
    - Changed from 2D array to a vector which better handles memory management.

--- The-Last-Pearl/code/LevelManager.cpp
+++ The-Last-Pearl/code/LevelManager.cpp
@@ -1,147 +1,99 @@
 #include "LevelManager.h"
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include <fstream>
+#include <stdexcept>
+#include <string>
+
+using namespace sf;
+using namespace std;
 
 std::vector<Vector2f> LevelManager::getWaypoints() const {
-	// Vector to store waypoints
-	std::vector<Vector2f> waypoints;
-	// Set waypoints
-	waypoints.push_back(Vector2f(100, 500));
-	waypoints.push_back(Vector2f(150, 500));
-	waypoints.push_back(Vector2f(200, 550));
-	waypoints.push_back(Vector2f(250, 500));
-	waypoints.push_back(Vector2f(300, 550));
-	waypoints.push_back(Vector2f(350, 500));
-	waypoints.push_back(Vector2f(400, 550));
-	waypoints.push_back(Vector2f(450, 500));
-	return waypoints;
+    // Vector to store waypoints
+    std::vector<Vector2f> waypoints;
+    // Set waypoints
+    waypoints.push_back(Vector2f(100, 500));
+    waypoints.push_back(Vector2f(150, 500));
+    waypoints.push_back(Vector2f(200, 550));
+    waypoints.push_back(Vector2f(250, 500));
+    waypoints.push_back(Vector2f(300, 550));
+    waypoints.push_back(Vector2f(350, 500));
+    waypoints.push_back(Vector2f(400, 550));
+    waypoints.push_back(Vector2f(450, 500));
+    return waypoints;
 }
 
 void LevelManager::SetLevel(int LevelNumber)
 {
-	
-	m_LevelSize.x = 0;
-	m_LevelSize.y = 0;
-	string leveltoload;
-	leveltoload = "Levels/Level1.txt";
+    // Load the level only if not already loaded / Preventing lag when trying to load the file over and over again
+    if (m_CurrentLevel == LevelNumber) return;
 
-	ifstream inputFile(leveltoload);
-	string s;
+    // Store current level number
+    m_CurrentLevel = LevelNumber;
+    m_LevelSize.x = 0;
+    m_LevelSize.y = 0;
 
+    // Create the level filename based on the level number
+    string levelToLoad = "Levels/Level" + std::to_string(LevelNumber) + ".txt";
+    std::ifstream inputFile(levelToLoad);
+    if (!inputFile) {
+        throw std::runtime_error("Cannot open level file: " + levelToLoad);
+    }
 
-	// Count the number of rows in the file
-	while (getline(inputFile, s))
-	{
-		++m_LevelSize.y;
-	}
-	//std :: cout << m_LevelSize.y<<"\n";
-	// Store the length of the rows
-	m_LevelSize.x = s.length();
+    // Load the level data into a vector
+    std::vector<std::vector<int>> arrayLevel;
+    std::string row;
+    while (getline(inputFile, row)) {
+        std::vector<int> rowData(row.length());
+        for (size_t x = 0; x < row.length(); ++x) {
+            rowData[x] = row[x] - '0'; // Assuming level data is single digits
+        }
+        arrayLevel.push_back(rowData);
+        ++m_LevelSize.y;
+    }
+    m_LevelSize.x = arrayLevel[0].size(); // Assuming all rows are the same length
 
-	// Go back to the start of the file
-	inputFile.clear();
-	inputFile.seekg(0, ios::beg);
+    // What type of primitive are we using?
+    rVaLevel.clear(); 
+    rVaLevel.setPrimitiveType(Quads);
+    // Set the size of the vertex array
+    rVaLevel.resize(m_LevelSize.x * m_LevelSize.y * VERTS_IN_QUAD);
 
-	// Prepare the 2d array to hold the int values from the file
-	int** arrayLevel = new int* [m_LevelSize.y];
-	for (int i = 0; i < m_LevelSize.y; ++i)
-	{
-		// Add a new array into each array element
-		arrayLevel[i] = new int[m_LevelSize.x];
-	}
+    //to et the space apart for screen size
 
-	// Loop through the file and store all the values in the 2d array
-	string row;
-	int y = 0;
-	while (inputFile >> row)
-	{
-		for (int x = 0; x < row.length(); x++) {
+    int ScreenxWidth = VideoMode::getDesktopMode().width;
+    int ScreenYHeight = VideoMode::getDesktopMode().height;
+    Tile_SizeX = (ScreenxWidth / m_LevelSize.x) / 2;
+    Tile_SizeY = (ScreenYHeight / m_LevelSize.y);
 
-			const char val = row[x];
-			//std:cout << val;
-			arrayLevel[y][x] = atoi(&val);
+    //std::cout << "hey " << ScreenxWidth << " / " << m_LevelSize.x << " = " << Tile_SizeX << " \n";
+    //::cout << "hey " << ScreenYHeight << " / " << m_LevelSize.y << " = " << Tile_SizeY << " \n";
+    
+    // Start at the beginning of the vertex array
+    int currentVertex = 0;
 
-		}
-		//std::cout << "\n";
-		y++;
-	}
-	
+    for (int x = 0; x < m_LevelSize.x; x++)
+    {
+        for (int y = 0; y < m_LevelSize.y; y++)
+        {
+            if (arrayLevel[y][x] != 0)
+            {
+                // Position each vertex in the current quad
+                rVaLevel[currentVertex + 0].position = Vector2f(x * Tile_SizeX, y * Tile_SizeY);
+                rVaLevel[currentVertex + 1].position = Vector2f((x * Tile_SizeX) + Tile_SizeX, y * Tile_SizeY);
+                rVaLevel[currentVertex + 2].position = Vector2f((x * Tile_SizeX) + Tile_SizeX, (y * Tile_SizeY) + Tile_SizeY);
+                rVaLevel[currentVertex + 3].position = Vector2f((x * Tile_SizeX), (y * Tile_SizeY) + Tile_SizeY);
 
-
-	// close the file
-	inputFile.close();
-
-	// What type of primitive are we using?
-	rVaLevel.setPrimitiveType(Quads);
-
-
-	int ScreenxWidth = VideoMode::getDesktopMode().width;
-	int ScreenYHeight = VideoMode::getDesktopMode().height;
-	// Set the size of the vertex array
-	rVaLevel.resize(ScreenxWidth * ScreenYHeight*VERTS_IN_QUAD );
-
-	//to et the space apart for screen size
-
-
-	Tile_SizeX = (ScreenxWidth / m_LevelSize.x)/2;
-	Tile_SizeY = (ScreenYHeight/m_LevelSize.y );
-
-	//std::cout << "hey " << ScreenxWidth << " / " << m_LevelSize.x << " = " << Tile_SizeX << " \n";
-	//::cout << "hey " << ScreenYHeight << " / " << m_LevelSize.y << " = " << Tile_SizeY << " \n";
-	// Start at the beginning of the vertex array
-	int currentVertex = 0;
-
-	for (int x = 0; x < m_LevelSize.x; x++)
-	{
-		for (int y = 0; y < m_LevelSize.y; y++)
-		{
-			if (arrayLevel[y][x] != 0)
-			{
-				// Position each vertex in the current quad
-				rVaLevel[currentVertex + 0].position =
-					Vector2f(x * Tile_SizeX, y * Tile_SizeY);
-
-				rVaLevel[currentVertex + 1].position =
-					Vector2f((x * Tile_SizeX) + Tile_SizeX, y * Tile_SizeY);
-
-				rVaLevel[currentVertex + 2].position =
-					Vector2f((x * Tile_SizeX) + Tile_SizeX, (y * Tile_SizeY) + Tile_SizeY);
-
-				rVaLevel[currentVertex + 3].position =
-					Vector2f((x * Tile_SizeX), (y * Tile_SizeY) + Tile_SizeY);
-
-				// Which tile from the sprite sheet should we use
-				//tile size times 1,2,3 this make the tile go down
-				//int check = arrayLevel[x][y];
-					//if (check == 5)
-					//	//{
-				//	arrayLevel[x][y] = 1;
-					//new array add pos
-				//}
-				int verticalOffset = arrayLevel[y][x] * PixelSize;
-			
-				//std::cout << "hey thingy " << arrayLevel[y][x]<<" && \n ";
-				//cout << " 1 x= " << x * Tile_SizeX << " y= " << y * Tile_SizeY;
-				//cout << "\n 2 x= " << (x * Tile_SizeX) + Tile_SizeX << " y= " << y * Tile_SizeY;
-				//cout << " \n3 x= " << (x * Tile_SizeX) + Tile_SizeX << " y= " << (y * Tile_SizeY) + Tile_SizeY;
-				// << " \n4 x= " << (x * Tile_SizeX) << " y= " << (y * Tile_SizeY) + Tile_SizeY;
-				rVaLevel[currentVertex + 0].texCoords =
-					Vector2f(0, 0 + verticalOffset);
-
-				rVaLevel[currentVertex + 1].texCoords =
-					Vector2f(PixelSize, 0 + verticalOffset);
-
-				rVaLevel[currentVertex + 2].texCoords =
-					Vector2f(PixelSize, PixelSize + verticalOffset);
-
-				rVaLevel[currentVertex + 3].texCoords =
-					Vector2f(0, PixelSize + verticalOffset);
-			}
-			// Position ready for the next four vertices
-			currentVertex = currentVertex + VERTS_IN_QUAD;
-		}
-	}
-
-
-
+                // Which tile from the sprite sheet should we use
+                //tile size times 1,2,3 this make the tile go down
+                int verticalOffset = arrayLevel[y][x] * PixelSize;
+                rVaLevel[currentVertex + 0].texCoords = Vector2f(0, 0 + verticalOffset);
+                rVaLevel[currentVertex + 1].texCoords = Vector2f(PixelSize, 0 + verticalOffset);
+                rVaLevel[currentVertex + 2].texCoords = Vector2f(PixelSize, PixelSize + verticalOffset);
+                rVaLevel[currentVertex + 3].texCoords = Vector2f(0, PixelSize + verticalOffset);
+            }
+            // Position ready for the next four vertices
+            currentVertex += VERTS_IN_QUAD;
+        }
+    }
 }
